d9747632a5e8bc955e3f99daf6c6b74b7f5d9469
Anatoly Tarnavsky <anatolyt@gmail.com>
2020-08-18 15:14:30 +0300

    Added websocket plugin

--- src/scripts/xterm.js
+++ src/scripts/xterm.js
@@ -1,22 +1,37 @@
 import { Terminal } from "xterm";
 import { FitAddon } from 'xterm-addon-fit';
+import { AttachAddon } from 'xterm-addon-attach';
 
 export default class Xterm {
 
     terminal;
     fitAddon;
+    attachAddon;
 
     constructor(containerId) {
-        this.terminal = new Terminal();
+        this.terminal = new Terminal({
+            cols: 40,
+            rows: 20,
+            cursorBlink: true,
+            cursorStyle: 'block',
+            fontSize: 20,
+            rendererType: 'canvas'
+        });
+
         this.fitAddon = new FitAddon();
         this.terminal.loadAddon(this.fitAddon);
+
+        const socket = new WebSocket('wss://docker.example.com/containers/mycontainerid/attach/ws');
+        this.attachAddon = new AttachAddon(socket);
+        this.terminal.loadAddon(this.attachAddon);
+
         this.terminal.open(document.getElementById(containerId));
         this.fit();
         this.write();
     }
 
     write() {
-        this.terminal.write('Hello from \x1B[1;3;31mxterm\n .js\x1B[0m $ ');
+        this.terminal.write('Welcome to anatoly.dev, type help to continue...');
     }
 
     fit () {
