fe77f3ff7e26d792bf1349faa5189bab98b8b944
Anatoly Tarnavsky <anatolyt@gmail.com>
2020-08-21 19:37:03 +0300

    Added control sequence commands which turn on/off alternate buffer

--- src/scripts/xterm.js
+++ src/scripts/xterm.js
@@ -6,7 +6,8 @@ export default class Xterm {
     terminal;
     fitAddon;
     socket;
-    currentLine = ''; // will store text till Enter pressed
+    loading = false;    // while loading command from server user input is blocked
+    currentLine = '';   // will store text till Enter pressed
 
     constructor(containerId) {
         this.terminal = new Terminal({
@@ -29,9 +30,6 @@ export default class Xterm {
         this.commandReset();
         this.prompt(false);
 
-        this.terminal.onResize((size) => {
-            console.log(size);
-        });
 
         this.terminal.attachCustomKeyEventHandler(keyEvent => {
            if (keyEvent.code.indexOf("Arrow") >= 0) {
@@ -39,12 +37,16 @@ export default class Xterm {
            }
         });
 
-        this.terminal.onKey(({ key, domEvent }) => {
+        this.terminal.onKey(async ({ key, domEvent }) => {
+            if (this.loading) {
+                return false;
+            }
+
             const code = key.charCodeAt(0);
             if (domEvent.ctrlKey && domEvent.key === 'c') { // ctrl+c
-                this.prompt(false);
+                await this.prompt(false);
             } else if (code === 13) {   // enter
-                this.prompt(true);
+                await this.prompt(true);
             } else if (code === 127) { // backspace
                 this.backspace();
             }else {
@@ -54,7 +56,7 @@ export default class Xterm {
         });
 
         this.terminal.onLineFeed(() => {
-            console.log('onLineFeed');
+
         });
 
         // this.socket = io('localhost:3001',{
@@ -68,14 +70,13 @@ export default class Xterm {
     }
 
     commandReset () {
-        console.log('reset');
         this.terminal.reset();
         this.terminal.write('Welcome to anatoly.dev, type cv to get my CV or help for more options...');
     }
 
-    prompt (process) {
+    async prompt (process) {
         if (process) {
-            this.processCurrentLine();
+            await this.processCurrentLine();
         }
         this.terminal.write("\r\n");
         this.terminal.write("anatoly.dev % ");
@@ -83,11 +84,37 @@ export default class Xterm {
     }
 
 
-    processCurrentLine () {
-        if (this.currentLine.trim() === 'reset') {
-            this.commandReset();
-        } else {
+    async asyncCommand () {
+        const sleep = m => new Promise(r => setTimeout(r, m));
+        this.loading = true;
+        const self = this;
+        let animationFrame;
+        function spinner() {
+            if (self.loading) {
+                self.terminal.write('n');
+                animationFrame = requestAnimationFrame(spinner);
+            }
+        }
+        spinner();
+        await sleep(500);
+        this.loading = false;
+    }
 
+    async processCurrentLine () {
+        const command = this.currentLine.trim();
+        if (command === 'reset') {
+            this.commandReset();
+        } else if (command === 'start') {
+            console.log('start');
+            this.terminal.write("\x9B?47h");
+        } else if (command === 'end') {
+            console.log('end');
+            this.terminal.write("\x9B?47l");
+        } else if (command === 'test') {
+            console.log('test');
+            this.terminal.write("\x9B2J");
+        } else {
+            // await this.asyncCommand();
         }
         this.currentLine = '';
     }
